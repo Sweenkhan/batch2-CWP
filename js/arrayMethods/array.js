f879a8062c528c99ff51dc1d4ba18ff1402a1031
Sween Khan <khansween@gmail.com>
2025-04-29 16:13:48 +0530

    learn adavnce loop and array method

---
+++ js/arrayMethods/array.js
@@ -0,0 +1,57 @@
+let names = ["rakesh", "rahul", "ajay", "ranjit", "priya", "akash"]
+
+
+// console.log(names)
+// console.log(names[0])        // "rakesh"        // giving first value in you array
+// console.log(names.length)    // 4               //its counting the items in your array
+// console.log(names[names.length-1] ) 
+
+
+// to add new value in our existing array,
+// push()
+
+names.push("soniya")
+names.push("rabia")
+
+
+// to remove last value from an array
+//pop()
+
+names.pop()
+names.pop()
+
+
+// to remove value from first index
+// shift()
+
+names.shift()
+names.shift()
+
+
+//to add value in first index
+names.unshift("chanchal", "minakshi")
+// console.log(names)
+
+
+
+
+
+// slice, splice, split, join 
+
+// slice           //slice(initialize, before the index)           //you can take only the values from the array
+// key point:   it will give you new value
+
+
+let names2 = ["rakesh", "rahul", "ajay", "ranjit", "priya", "akash"]
+let sliceNum = names2.slice(2, 4)
+
+// console.log(names2.slice(4))
+// console.log(sliceNum)
+
+ 
+//splice       splice(startingIndex, how many value you want to remove,   adding new values)
+// key point:  it will change your original array 
+
+ names2.splice(2, 3) 
+ console.log(names2)
+
--- js/loops/forLoop.js
+++ js/loops/forLoop.js
@@ -36,26 +36,73 @@ let greet = `hello ${name}, welcome to our website. because you age is ${age}, y
 
 // asking user for a number. based on that number giving back him a table
 
- let number = prompt("give a number, which you want to see the table")
-let stringToNum = Number(number) 
+//  let number = prompt("give a number, which you want to see the table")
+// let stringToNum = Number(number) 
 
-for(let i = 1;  i <= 100; i++){
+// for(let i = 1;  i <= 100; i++){
 
-    // console.log(stringToNum * i)
-    console.log(`${stringToNum} x ${i} = ${stringToNum * i}`)
+//     // console.log(stringToNum * i)
+//     console.log(`${stringToNum} x ${i} = ${stringToNum * i}`)
 
-}
-
-
+// }
 
 
 let greeting ="jgjgjkvj hello how are you gkkgkg";
 
 
-for(let i = 100; i >= 1 ; i--){
-    // console.log(i, greeting)
-    console.log(i)
+// for(let i = 100; i >= 1 ; i--){
+//     // console.log(i, greeting)
+//     console.log(i)
+// }
+
+
+let firstName = "sween"
+
+for(let count = 1; count <= 100; count++){
+    // console.log(new Date().getMilliseconds())
 }
+
+
+
+// --------------------------------------------loops in advance--------------------------------------------
+let names = ["rakesh", "rahul", "ajay", "ranjit", "priya", "akash"]
+
+
+// for(let i = 0; i < names.length; i++){
+//      console.log(names[i])
+// }
+
+
+
+// for(let i = 0; i < names.length; i++){
+
+//          if(names[i] === "ajay"){
+//             console.log("ajay kumar")
+//          } else {
+//             console.log(names[i])
+//          }
+     
+//     }
+
+
+// continue property                                 //to skip current task while running loop
+// for(let i = 0; i < names.length; i++){
+
+//     if(names[i] === "ajay"){
+//        continue;
+//     } else {
+//        console.log(names[i])
+//     }
+// }
+
+
+
+ 
+
+
+
+ 
+ 
 
 
 
