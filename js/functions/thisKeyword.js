ab2460d990cd3250d0cc0c56666276d0356957b9
Sween Khan <khansween@gmail.com>
2025-05-05 16:15:46 +0530

    learn about dom

--- js/functions/thisKeyword.js
+++ js/functions/thisKeyword.js
@@ -1,21 +1,35 @@
 // console.log(this) only work with function expression inside an object
 
 
+// let profile = {
+//     firstName: "kiran",
+//     lastName: "kher",
+//     age: 66, 
+
+//     fullName: function(){
+//          return this.firstName + " "+ this.lastName
+//     },
+//     about: function(){
+//         return `hello i am ${this.firstName}, and i am ${this.age} years old. i want to work with you`
+//     }
+// }
+
+
 let profile = {
-    firstName: "kiran",
-    lastName: "kher",
+    firstName: "rahul",
+    lastName: "sharma",
     age: 66, 
 
     fullName: function(){
-         return this.firstName + " "+ this.lastName
+            console.log(this.firstName + " " + this.lastName)
     },
-    about: function(){
-        return `hello i am ${this.firstName}, and i am ${this.age} years old. i want to work with you`
+
+    greet: function(){
+        console.log(`hello ${this.firstName} ${this.lastName}, welcome to our platform`)
     }
+    
 }
 
- 
+profile.fullName()
+ profile.greet()
 
-
-
-console.log(profile.about())
