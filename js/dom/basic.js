8d0913fac80067f2a4ce40a9b563dc7f7310fca5
Sween Khan <khansween@gmail.com>
2025-05-08 16:42:08 +0530

    learn about dom event

--- js/dom/basic.js
+++ js/dom/basic.js
@@ -5,6 +5,7 @@ let heading1 = document.getElementsByTagName("h2")[0]                        //a
 let heading2 = document.getElementsByClassName("list")
 let heading3 = document.getElementsByName("f-heading")[0]
 
+
 let input = document.querySelector("input[type='password']")
 
  input.style.padding = "5px"
@@ -36,8 +37,27 @@ list.style.color = "green"
 // innerHTML,innerText, textContent   (difference between this)
 
 // console.log(para.innerHTML)               //text and if tag is available that will also come
-// console.log(para.innerText)               //onlu you will get the text content
+// console.log(para.innerText)               //only you will get the text content
 // console.log(para.textContent)             //you will get all the context, does'nt matter if its not visible
 
 
 // para.innerText = "hi, today's class is over. and we have learned about dom. "
+list.innerHTML = `<span>welcome</sapn> to our website`
+
+ list.innerHTML = ""              //remove elements
+
+
+
+// notes :
+
+// accessing Elements in dom
+
+// upadating elements text
+// remove Elements from dom
+// creating elements
+
+// adding style through dom
+// adding class (remove, delete, replace)
+// adding attributes (add, remove, replace)       atrr(src, id, class, href, width, height)
+
+
