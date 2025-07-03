e8415f5fb07c1b641c8761453a1c36b6b72e4a9f
Sween Khan <khansween@gmail.com>
2025-07-03 12:16:27 +0530

    to do list 2nd half

--- react/05vite-todo-app/src/App.jsx
+++ react/05vite-todo-app/src/App.jsx
@@ -4,50 +4,84 @@ import './App.css'
 
 function App() {
 
-  const [task, setTask] = useState("")
-  const [list, setList] = useState([])
-
-
-  function handleClick() {
-
-    setList((prev) =>
-      [...prev, task]
-    )
-    setTask("")
-  }
-
-
-  function handleDelete(li){
-      
-    let filterData =   list.filter((lis) => lis !== li)
-
-   setList(filterData)
-   console.log(filterData)
-  }
+  const [task, setTask] = useState("")                    //input value
+  const [tasks, setTasks] = useState([])
+  const [editStart, setEditStart] = useState(false)
+  const [editItemId, setEditItemId] = useState("")
  
+ 
+function handleAdd(){
+
+  if(editStart){
+
+   let filterData = tasks.map((item) => item.id === editItemId ? item.text = task : item)
+ 
+    // console.log(filterData)
+    setEditItemId("")
+    setEditStart(false)
+
+  } else {
+  
+
+     setTasks((prev) => 
+    [...prev, {
+
+      id: Date.now(),
+      text: task,
+      completed: false
+    }]
+)
+
+  }
+
+  
+  
+//changing input value to empty
+setTask("") 
+}
+
+ 
+
+function handleDelete(id){
+  // console.log(id)
+ let filterData =   tasks.filter((item) => item.id !== id )
+ setTasks(filterData)
+}
+
+
+
+function handleEdit(id){
+  setEditItemId(id)
+  setEditStart(true)
+  let filterData = tasks.filter((item) => item.id == id )
+
+  setTask(filterData[0].text)
+
+}
+
 
   return (
 
     <div className='todo-app'>
-      <div className='tod0-container'>
+
+      <div className='todo-container'>
         <h1>My To-Do List</h1>
 
-        <div> 
-          <input style={{ padding: "5px", fontSize: "1.2rem" }}
-            type='text' placeholder='Add new task' onChange={(e) => setTask(e.target.value)} value={task} /> 
-          <button onClick={handleClick}>Add</button> 
-        </div>
- 
+        <div>  
+           <input type='text' placeholder='enter your task' onChange={(e) => setTask(e.target.value.toUpperCase())}  value={task} />
+           <button onClick={handleAdd}>Add</button> 
+        </div> 
+
         <div className='dropdown-list'> 
 
-          { (list.length > 0) && 
-          list.map((li, index) => { 
-            return (
-              <p key={index}> {li} <span style={{padding: "0 15px"}} onClick={() => handleDelete(li) }> delete</span> </p>
-            )
-          }) 
-        }  
-
+         {
+             tasks.map((item) => {
+              return (
+                  <p key={item.id}> {item.text}   <span style={{paddingLeft: "70px"}} onClick={() => handleDelete(item.id)}>delete</span> <span onClick={ () => handleEdit(item.id)}>edit</span> </p>
+              )
+             })
+         }
+   
         </div>
 
       </div>
