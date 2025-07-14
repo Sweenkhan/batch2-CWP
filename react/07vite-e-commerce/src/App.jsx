cbad4986f6a14b76b5020e4d5f96cea3f2e5abb1
Sween Khan <khansween@gmail.com>
2025-07-14 12:26:17 +0530

    learn about form handling and navigation in react

--- react/07vite-e-commerce/src/App.jsx
+++ react/07vite-e-commerce/src/App.jsx
@@ -5,19 +5,22 @@ import Products from "./pages/Products"
 import SignUp from "./pages/SignUp"
 import About from "./pages/About"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Register from "./pages/Register"
 
 
 function App() {
 
   return (
    <div style={{minHeight: "100vh"}}> 
+   <div className="max-w-[1460px]"></div>
       <BrowserRouter >
 
       <Header />
         <Routes >
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} /> 
-          <Route path="/sign-up" element={<SignUp />} />
+          {/* <Route path="/sign-up" element={<SignUp />} /> */}
+          <Route path="/sign-up" element={<Register />} />
           <Route path="/about" element={<About />} />
         </Routes>
       <Footer />
--- react/07vite-e-commerce/src/pages/Home.jsx
+++ react/07vite-e-commerce/src/pages/Home.jsx
@@ -7,9 +7,9 @@ function Home() {
   return (
     <> 
     
-    <div className='container'>
-        <div className='relative '>
-            <img src={heroImage} alt='hero'/>
+    <div className=' w-full'>
+        <div className='relative w-full'>
+            <img src={heroImage} alt='hero'  />
             <div className='absolute right-[6rem] bottom-[8rem] w-1/4'>
                 <h2 className='text-white text-3xl pb-3 font-bold'>Smart Shopping Trusted by Millions</h2>
                 <button className='px-3 py-1 bg-white rounded-2xl '>Shop Now</button>
